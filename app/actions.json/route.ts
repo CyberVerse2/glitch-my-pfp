18c8696fe0b4041aefec831e1980d522afff2509
Ejiofor Celestine Chinaza <121381551+CyberVerse2@users.noreply.github.com>
2024-09-03 12:43:02 +0100

    fixed actions

--- app/actions.json/route.ts
+++ app/actions.json/route.ts
@@ -1,26 +1,27 @@
-import { createActionHeaders, type ActionsJson } from '@solana/actions';
+import { ACTIONS_CORS_HEADERS, ActionsJson } from '@solana/actions';
 
 export const GET = async () => {
   const payload: ActionsJson = {
     rules: [
-      // map all root level routes to an action
+      {
+        pathPattern: '/',
+        apiPath: '/api/actions'
+      },
       {
         pathPattern: '/*',
         apiPath: '/api/actions/*'
       },
-      // idempotent rule as the fallback
+      // fallback route
       {
-        pathPattern: '/api/actions/**',
-        apiPath: '/api/actions/**'
+        pathPattern: '/api/actions',
+        apiPath: '/api/actions'
       }
     ]
   };
 
   return Response.json(payload, {
-    headers: createActionHeaders()
+    headers: ACTIONS_CORS_HEADERS
   });
 };
-
-// DO NOT FORGET TO INCLUDE THE `OPTIONS` HTTP METHOD
-// THIS WILL ENSURE CORS WORKS FOR BLINKS
+// ensures cors
 export const OPTIONS = GET;
