c763407be2054554e49a9fd9ef6deffbb8fe6f29
Ejiofor Celestine Chinaza <121381551+CyberVerse2@users.noreply.github.com>
2024-09-04 05:04:37 +0100

    deploy

--- app/api/actions/glitch-my-pfp/route.ts
+++ app/api/actions/glitch-my-pfp/route.ts
@@ -41,7 +41,6 @@ import {
   publicKey
 } from '@metaplex-foundation/umi';
 
-
 fal.config({
   credentials: process.env.FAL_AI_API_KEY
 });
@@ -51,7 +50,7 @@ const client = new BlinksightsClient(process.env.BLINKSIGHTS_API_KEY!);
 const headers = createActionHeaders();
 
 const SEND_TOKEN_ADDRESS = new PublicKey('SENDdRQtYMWaQrBroBrJ2Q53fgVuq95CV9UPGEvpCxa');
-const RECIPIENT_ADDRESS = new PublicKey('E5HmSiV9XjnGj6y9KogyHx3U7Q9GzcpRfRZrwosqEL8A');
+const RECIPIENT_ADDRESS = new PublicKey('6kexz7VwA5J895tdWaDP6b4S9okQez1Att6E2jzWLXMk');
 
 async function generateImage(prompt: string, isUltra: boolean): Promise<string> {
   const model = isUltra ? 'fal-ai/flux-realism' : 'fal-ai/flux/schnell';
@@ -173,7 +172,7 @@ export async function POST(req: NextRequest) {
     } catch (err) {
       throw 'Invalid "account" provided';
     }
-    await client.trackActionV2(account as unknown as string, req.url)
+    await client.trackActionV2(account as unknown as string, req.url);
 
     const { searchParams } = new URL(req.url);
     console.log(searchParams);
@@ -197,16 +196,18 @@ export async function POST(req: NextRequest) {
     //   account as unknown as string,
     //   req.url
     // );
-    const connection = new Connection(process.env.SOLANA_RPC! || clusterApiUrl('mainnet-beta'));
+    const connection = new Connection(process.env.SOLANA_RPC!, 'confirmed');
 
     // Get the associated token addresses
     const fromTokenAddress = await getAssociatedTokenAddress(SEND_TOKEN_ADDRESS, account);
     const toTokenAddress = await getAssociatedTokenAddress(SEND_TOKEN_ADDRESS, RECIPIENT_ADDRESS);
+    console.log(fromTokenAddress, toTokenAddress);
 
     const transaction = new Transaction();
 
     // Check if the recipient's token account exists, if not, create it
     const toTokenAccount = await connection.getAccountInfo(toTokenAddress);
+    console.log(toTokenAccount);
     if (!toTokenAccount) {
       transaction.add(
         createAssociatedTokenAccountInstruction(
@@ -219,7 +220,7 @@ export async function POST(req: NextRequest) {
     }
 
     // Add transfer instruction
-    const imageGenerationCost = isUltra ? 20000000 : 1000000; // calculate cost based on isUltra
+    const imageGenerationCost = isUltra ? 20000000 : 10000000; // calculate cost based on isUltra
     transaction.add(
       createTransferInstruction(
         fromTokenAddress,
@@ -236,6 +237,8 @@ export async function POST(req: NextRequest) {
 
     // Get the latest blockhash
     transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+
+    // Sign the transaction
 
     const payload = await createPostResponse({
       fields: {
